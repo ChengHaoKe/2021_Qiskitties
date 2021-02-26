d5de1306ad06687de5cc33b244ad2ea748a850b6
ChengHaoKe <102256006c@gmail.com>
2021-02-25 20:46:44 -0600

    commented out pca

--- analysis/CH_qiskit.py
+++ analysis/CH_qiskit.py
@@ -173,10 +173,10 @@ std_scale = StandardScaler().fit(sample_train)
 sample_train = std_scale.transform(sample_train)
 sample_test = std_scale.transform(sample_test)
 
-# Now reduce number of features to number of qubits
-pca = PCA(n_components=n).fit(sample_train)
-sample_train = pca.transform(sample_train)
-sample_test = pca.transform(sample_test)
+# # Now reduce number of features to number of qubits
+# pca = PCA(n_components=n).fit(sample_train)
+# sample_train = pca.transform(sample_train)
+# sample_test = pca.transform(sample_test)
 
 # Scale to the range (-1,+1)
 samples = np.append(sample_train, sample_test, axis=0)
